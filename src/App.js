7b140afbb82401b936c1eb4cb322621679c5ff55
Roshan panda <roshanpanda666@gmail.com>
2023-02-18 19:17:49 +0530

    upgrad

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ function App() {
   const[lightModeButton,DarkModeButton]=useState("lightBtn")
   const[lighttxt,darktxt]=useState("light mode")
   const[txt,styletxt]=useState("lightmodetext")
+  const[game,gamestyle]=useState("gametxt")
 
   const dark=()=>{
     setStyle("cont2")
     DarkModeButton("darkBtn")
     darktxt("dark mode")
     styletxt("darkmodetxt")
+    gamestyle("gamestyletxt")
   }
 
   return (
@@ -21,7 +23,9 @@ function App() {
 
         <h1 className={txt}>{lighttxt}</h1>
         <button className={lightModeButton} onClick={dark}>click !</button>
-        
+        <div className={game}>
+        <br></br>enjoy the gaming experience more with Dark mode
+        </div>
       </div>
     </div>
   );
