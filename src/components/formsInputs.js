c4ccd75c59bb665e9657a3e2a376c5b1c6b34388
Roshan panda <roshanpanda666@gmail.com>
2023-02-18 21:48:44 +0530

    commiting2ndtime

--- src/components/formsInputs.js
+++ src/components/formsInputs.js
@@ -5,23 +5,18 @@ export default function FormsInputs(props) {
 
     const[title,seTtitle]=useState('')
 
-    const changeEvent=(event)=>{
-        
-        console.log(event.target.value)
-        seTtitle(event.target.value)
-    }
 
     const onSubmit=(event)=>{
         event.preventDefault()
-        alert(title)
-        
+
+        seTtitle(event.target.value)
     }
     
 
   return (
     <div>
       <form onSubmit={onSubmit} className="fathercont" >
-        <input type="text" className='TxtForm' onChange={changeEvent}/>
+        <input type="text" className='TxtForm' value={title}/>
         <div className="titleC">
             {title}
         </div>
